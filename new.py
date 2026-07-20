d73c5c8408775905052927c93a3abdc5164f4add
bhavanimohan <bhavanimohan200@gmail.com>
2026-07-20 20:51:18 +0530

    Just had an hands-on-experience

---
+++ new.py
@@ -0,0 +1,23 @@
+# a =input()
+# b = input()
+# c= int(input())
+# d = int(input())
+
+# print(a+ " "+ b)
+# print(c+d)
+
+# a = input("Enter your name : ")
+# b = int(input("Enter the value that you want to print : "))
+# print(a + str(b))
+name = "nani"
+age = 22
+print("my name is %s my age is %d" %(name, age))
+print()
+
+name = input()
+stream = input()
+clg = input()
+native = input()
+cgpa = input()
+print(f"Hello This is {name}. I'm from {native}. I had completed my studies in {stream} from {clg} with an cpga of{cgpa}.")
+
---
+++ string_methods/new.py
@@ -0,0 +1,18 @@
+a = "Bhavani Mohan"
+print(a.upper()) # Converts the string to uppercase
+
+print(a.lower()) # Converts the string to lowercase
+
+print(a.title()) # Converts the string to title case
+
+print(a.isupper()) # Checks if the string is in uppercase
+
+print(a.islower()) # Checks if the string is in lowercase
+
+print(a.istitle()) # Checks if the string is in title case
+
+print(a.startswith("Bha")) # Checks if the string starts with "Bha"
+
+print(a.endswith("Mohan")) # Checks if the string ends with "Mohan"
+
+print(a.replace("Mohan", "Kumar")) # Replaces "Mohan" with "Kumar"
