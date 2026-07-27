095c9246ca3cbca734b1cd460434a00aaf64cc19
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-07-27 13:41:37 +0530

    Nested if-else examples were added in this

--- app.py
+++ app.py
@@ -1,12 +1,58 @@
-a =int(input("Enter the number :"))
+# #Greater than a number or not
+# a =int(input("Enter the number :"))
 
-if(a>=100):
-    print("Greater than 100")
-else:
-    print("Less than 100")
+# if(a>=100):
+#     print("Greater than 100")
+# else:
+#     print("Less than 100")
+
+# #Even number or not  
+# a =int(input("Enter a value "))
+
+# if(a%2==0):
+#     print(f"The entered value {a} is  even number")
+# else:
+#     print(f"The entered value {a} is  odd number")
     
-a =int(input("Enter a value "))
-if(a%2==0):
-    print(f"The entered value {a} is  even number")
+    
+# a = int(input("Enter the number "))
+
+
+#if(((a**2)%10)==a):
+#     print("it's a  number")
+# else:
+#     print("not  number")
+
+a=int(input())
+is_voter_ID = input()
+is_indian = input()
+
+# if(a>=18):
+#     if(bool(is_indian) == True):
+#         if(bool(is_voter_ID)==True):
+#             print("Eligible to vote")
+#         else:
+#             print("Not eligible to vote bcz of voter_ID")
+        
+#     else:
+#         print("not eligible to vote bcz of not an indian")
+# else:
+#     print("Not eligible to vote")
+    
+    
+    
+        
+if(a>=18):
+    print("eligible to vote")
 else:
-    print(f"The entered value {a} is  odd number")
+    print(" not eligible to vote")
+        
+if(bool(is_indian == True)):
+    print("eligible to vote")
+else:
+    print(" not eligible to vote bcz of bcz of not an indian")
+        
+if(bool(is_voter_ID == True)):
+    print("eligible to vote")
+else:
+    print(" not eligible to vote bcz of not having voter id")
