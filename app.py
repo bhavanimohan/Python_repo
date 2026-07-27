b8fd81f52fbd98831c9e19d9ac589220852cded3
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-07-27 12:39:23 +0530

    Conditinal Statements

---
+++ app.py
@@ -0,0 +1,12 @@
+a =int(input("Enter the number :"))
+
+if(a>=100):
+    print("Greater than 100")
+else:
+    print("Less than 100")
+    
+a =int(input("Enter a value "))
+if(a%2==0):
+    print(f"The entered value {a} is  even number")
+else:
+    print(f"The entered value {a} is  odd number")
