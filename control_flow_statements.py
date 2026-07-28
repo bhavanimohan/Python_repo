f3ada5e662f0ff4ac70695eba7875ce9289e1467
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-07-28 13:23:55 +0530

    Logical operators using if-elif-else conditions

--- control_flow_statements.py
+++ control_flow_statements.py
@@ -19,11 +19,65 @@
 #     print("Problem in tv")
 
 
-age = int(input("Enter your age : "))
-salary = int(input("Enter your salary : "))
-has_pf = bool(input("he is having pf or not : "))
+# age = int(input("Enter your age : "))
+# salary = int(input("Enter your salary : "))
+# has_pf = bool(input("he is having pf or not : "))
 
-if(age>=20 and salary >20000 and has_pf):
-    print("Yes")
+# if(age>=20 and salary >20000 and has_pf):
+#     print("Yes")
+# else:
+#     print("No") 
+
+# a = int(input("Do you have pf :(Yes : Type :1 / No : Type : 0"))
+# if(a):
+#     print("Yes")
+# else:
+#     print("No")
+    
+    
+# has_id = bool(input("Do you have id : "))
+# has_pass = bool(input("Do you have pass : "))
+# if(has_id or has_pass):
+#     print("Allowed to class")
+# else:
+#     print("Not Allowed to class")    
+
+# is_innomatian = bool(input("Is he belongs to innomatics  : "))
+# has_id = bool(input("Do you have id : "))
+# has_pass = bool(input("Do you have pass : "))
+# if(is_innomatian and (has_id or has_pass)):
+#     print("Allowed to class")
+# else:
+#     print("Not Allowed to class") 
+    
+# ScoreA = int(input("Score of team A : "))
+# ScoreB = int(input("Score of team B : "))
+# if(ScoreA > ScoreB):
+#     print("Team A wins")
+# elif(ScoreA < ScoreB):
+#     print("Team B wins")
+# else:
+#     print("Draw")
+
+
+# number = int(input("Enter the number : "))
+
+# if(number % 3 == 0 and number % 5 == 0):
+#     print("FizzBuzz")
+# elif (number % 5 == 0):
+#     print("Buzz")
+# else:
+#     print("Fizz")
+
+
+marks = int(input("Enter your marks : "))
+if(marks>=80):
+    print("Excellent")
+elif (marks>=71 and marks<=80):
+    print("A")
+elif(marks>=61 and marks<=70):
+    print("B")
+elif(marks>=51 and marks<=60):
+    print("C")
 else:
-    print("No") 
+    print("Fail")
