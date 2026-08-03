dd7338c3c94f2e5b6efc1501383e94f4707be4e9
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-08-03 13:11:54 +0530

    When to Turn ON / OFF is updated in this module

---
+++ 09_while_loops/while_Loops.py
@@ -0,0 +1,27 @@
+
+# a = int(input("Enter a number: "))
+# i=1
+# while i <10:
+#     print(i)
+#     i+=1
+#     print(i)
+
+# count  = 100
+# while (count<200):
+    
+#     if(count % 5 ==0):
+#         print(count)
+#     count+=1
+        
+        
+    
+a = int(input("Enter a number: "))
+b = 1
+while(b):
+    if(a%2==0):
+        print("Congratulations you won")
+        b = 0
+        
+    else:
+        print("You loose try again")
+        a= int(input("Enter a number: "))
