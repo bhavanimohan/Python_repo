43ff96a3444ceb41d4805f27186b70af2a7c7b9c
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-08-12 12:37:10 +0530

    arguments using for loops / functions

--- arguments/arguments.py
+++ arguments/arguments.py
@@ -10,13 +10,38 @@
 #     print(f"Age : {age}")
 #     print(f"Course : {course}")
 # details(name = "nani",age = 23,course="Python")
-
 #these are called as key-value pair arguments 
 
 
 
-def details(name = "amul"):
-    print(f"Name : {name}")
-    # print(f"Age : {age}")
-    # print(f"Course : {course}")
-details()
+# def details(name = "amul"):
+#     print(f"Name : {name}")
+#     # print(f"Age : {age}")
+#     # print(f"Course : {course}")
+# details()
+#these are the default values that are automatically takes name as amul if don't mention any name in the function call.
+
+# def my_average(*a):
+#     print(f"values : {a}")
+#     sums = sum(a)
+#     lens = len(a)
+#     print(sums/lens)
+
+
+  
+# my_average(1,2)
+
+
+def my_average(*a):
+    print(f"values : {a}")
+    count = 0
+    countss =0
+    for char in a:
+        count+=char
+        countss+=1
+    print(count/countss)
+        
+
+
+  
+my_average(1,2)
