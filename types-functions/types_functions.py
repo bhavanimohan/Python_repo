27c025f705d8f96df18a33c242420449530f24d4
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-08-18 14:04:08 +0530

    done with type-functions

--- types-functions/types_functions.py
+++ types-functions/types_functions.py
@@ -22,6 +22,17 @@ print(print_upper("nani"))
 even_odd = lambda num : "even" if num % 2 == 0 else "odd"
 print(even_odd(6))
 
-max_of_two = lambda a,b: max(a,b)
-print(max_of_two(4,7))
+max_of_two = lambda a,b: a if(a>b) else b
+print(max_of_two(8,7))
 
+positive_negative = lambda a : "Positive " if (a>0) else "Negative" if a < 0 else "Equals to zero"
+print(positive_negative(0))
+
+pass_marks = lambda a: "pass" if a>60 else "fail"
+print(pass_marks(75))
+
+# marks = lambda :  b = b.sort()
+# print(marks([2,5,6,9,4,2,1,8,5,7,2,9,7]))
+marks = [("nani",75),("anu",90),("hari",95),("chandu",89)]
+new_marks = sorted(marks)
+print(new_marks)
