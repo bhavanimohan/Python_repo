d7a985922fc027e9bf08f66a4d374d6929d2ea6c
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-08-18 12:29:39 +0530

    done some more problems on type_functions

--- types-functions/types_functions.py
+++ types-functions/types_functions.py
@@ -1,10 +1,27 @@
-# def sq_num(num):
-#     res = num **2
-#     return res
-# print(sq_num(5))
+def sq_num(num):
+    res = num **2
+    return res
+print(sq_num(5))
 
 sq_num = lambda num : num **2
 print(sq_num(5))
 
 sum = lambda a,b : a+b
 print(sum(2,4))
+
+length = lambda b : len(b)
+print(length("bhavani mohan"))
+
+plus_10 = lambda b : 10+b
+print(plus_10(35))
+
+print_upper =lambda b : b.upper()
+print(print_upper("nani"))
+
+
+even_odd = lambda num : "even" if num % 2 == 0 else "odd"
+print(even_odd(6))
+
+max_of_two = lambda a,b: max(a,b)
+print(max_of_two(4,7))
+
