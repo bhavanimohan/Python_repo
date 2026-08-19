cdfa4fbbc25bf20a03a81a257f694750af8d2a96
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-08-19 10:05:21 +0530

    yesterday's class had been re-capped

--- arguments/problems_from_chatgpt/chatgpt_problems_on_functions.py
+++ arguments/problems_from_chatgpt/chatgpt_problems_on_functions.py
@@ -58,18 +58,33 @@
 #     return empty_list
 # print(remove_duplicates(1,2,3,1,2,3,4,5,6))
 
-def analyze_number(n):
-    res = ""
-    if n > 0 and n % 2 == 0:
-        res = "Positive Even"
-    elif n>0 and n%2 != 0:
-        res = "Positive Odd"
-    elif n<0 and n % 2 == 0:
-        res = "Negative Even"
-    elif n<0 and n%2!= 0:
-        res = "Negative Odd"
-    else:
-        res = "Zero"
-    return res
-print(analyze_number(-7))
+# def analyze_number(n):
+#     res = ""
+#     if n > 0 and n % 2 == 0:
+#         res = "Positive Even"
+#     elif n>0 and n%2 != 0:
+#         res = "Positive Odd"
+#     elif n<0 and n % 2 == 0:
+#         res = "Negative Even"
+#     elif n<0 and n%2!= 0:
+#         res = "Negative Odd"
+#     else:
+#         res = "Zero"
+#     return res
+# print(analyze_number(-7))
 
+
+# def frequency(numbers):
+#     count = {}
+#     for char in numbers:
+#         if char not in count:
+#             count[char] = numbers.count(char)
+#     return count
+# print(frequency([1,2,3,4,5,3,2,1,1,2,3]))
+
+#yesturday's class lamda just a re-cap
+sums = lambda a,b : a+b
+print(sums(2,3))
+#if else condtional statements checking 
+even = lambda a : "Even" if a % 2 == 0 else "Odd"
+print(even(9))
