c8b8212478baa154c1a4cda88ca2548518d92057
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-08-20 19:56:32 +0530

    done with 15 problems

--- higher_order_functions/Maps_task.py
+++ higher_order_functions/Maps_task.py
@@ -26,10 +26,34 @@
 # a = list(map(lambda x : len(x),words))
 # print(a)
 
-nums = [1, 2, 3]
-a = list(map(lambda x : str(x),nums))
+# nums = [1, 2, 3]
+# a = list(map(lambda x : str(x),nums))
+# print(a)
+
+# celsius = [0, 20, 37, 100]
+# a = list(map(lambda x : (x*(9/5))+32,celsius))
+# print(a)
+
+# prices = [100, 250, 500]
+# a = list(map(lambda x :x - x * 10/100,prices))
+# print(a)
+
+# nums = [3, 4, 7, 10]
+# a = list(map(lambda x : "Even" if x % 2 == 0 else "Odd",nums))
+# print(a)
+
+marks = [55, 60, 45]
+a = list(map(lambda x : x +5,marks))
 print(a)
 
-celsius = [0, 20, 37, 100]
-a = list(map(lambda x : (x*(9/5))+32,celsius))
+names = ["Dev", "Meera"]
+a = list(map(lambda x  : "Hello "+ x,names))
+print(a)
+
+nums = [10, 14, 20]
+a = list(map(lambda x : x % 3 , nums))
+print(a)
+
+ages = [20, 25, 30]
+a = list(map(lambda x : x + 1,ages))
 print(a)
