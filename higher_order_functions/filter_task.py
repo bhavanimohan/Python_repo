11098fe59b3a50050498d3d103ec246947929d25
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-08-22 00:35:20 +0530

    done with 15 problems

--- higher_order_functions/filter_task.py
+++ higher_order_functions/filter_task.py
@@ -16,17 +16,43 @@
 # Input = [(1, 2), (-3, 4), (5, -6), (7, 8)]
 # print(list(filter(lambda x : (x[0] > 0 and x[1]> 0),Input)))
 
-Input = [('Pen', 20), ('Watch', 1500), ('Bag', 450)]
-print(list(filter(lambda x : (x[1]<500),Input)))
+# Input = [('Pen', 20), ('Watch', 1500), ('Bag', 450)]
+# print(list(filter(lambda x : (x[1]<500),Input)))
 
-Input = [(3, 3), (2, 5), (7, 7), (1, 2)]
-print(list(filter(lambda x : (x[0] == x[1]),Input )))
+# Input = [(3, 3), (2, 5), (7, 7), (1, 2)]
+# print(list(filter(lambda x : (x[0] == x[1]),Input )))
 
-Input = [('Apple', 1), ('Mango', 2), ('Orange', 3)]
-print(list(filter(lambda x : (x[0][0] in "aeiouAEIOU") , Input)))
+# Input = [('Apple', 1), ('Mango', 2), ('Orange', 3)]
+# print(list(filter(lambda x : (x[0][0] in "aeiouAEIOU") , Input)))
 
-Input = [('Mon', 32), ('Tue', 38), ('Wed', 40)]
-print(list(filter(lambda x : (x[1] > 35),Input)))
+# Input = [('Mon', 32), ('Tue', 38), ('Wed', 40)]
+# print(list(filter(lambda x : (x[1] > 35),Input)))
+
+Input = [('cat', 1), ('bird', 2), ('ant', 3), ('fish', 4)]
+print(list(filter(lambda x : len(x[0]) % 2 ==0 ,Input)))
+
+Input = [(1, 10), (2, 7), (3, 15), (4, 8)]
+print(list(filter(lambda x : (x[0] % 5 ==0) or (x[1] % 5 == 0) , Input )))
+
+Input = [(5, 2), (1, 4), (7, 7), (2, 9)]
+print(list(filter(lambda x : (x[0] < x[1]), Input)))
+
+Input = [('Banana', 1), ('Sky', 2), ('Grape', 3)]
+print(list(filter(lambda x : "a" in x[0],Input)))
+
+Input = [(2, 'a'), (4, 'b'), (7, 'c'), (9, 'd'), (11, 'e')]
+def is_prime(x):
+    count = True
+    for i in range(2,int(x[0])):
+        if x[0] % i == 0:
+            count = False
+    return count
 
 
+print(list(filter(lambda x : is_prime(x),Input)))
 
+Input= [{'name': 'Ravi', 'salary': 60000},
+{'name': 'Anu', 'salary': 40000}
+]
+
+print(list(filter(lambda x : x[1]>50000 ,Input)))
