0bd2d253196067dcb85e33304387112858cf4f6f
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-08-21 12:53:52 +0530

    we had seen couple of problem's on filter's

--- higher_order_functions/filter.py
+++ higher_order_functions/filter.py
@@ -1,3 +1,16 @@
 a = ["nani","rajesh","jai","suresh","naresh"]
 b = list(filter(lambda x : len(x) > 4 , a))
 print(b)
+
+names = ["nani","aravind","ganesh","rajesh","jai","abhi","mahesh","avinash"]
+print(list(filter(lambda x : x.startswith("a") ,names)))
+
+names = ["nani","aravind","ganesh","rajesh","jai","abhi","mahesh","avinash"]
+print(list(filter(lambda x : x.endswith("sh") ,names)))
+a = [1,-8,-4,7,8,5,-10,-6]
+print(list(filter(lambda x : x>0 ,a )))
+print(list(filter(lambda x : x<0 ,a )))
+
+
+a = [1,-8,-4,7,8,5,-10,-6]
+print(list(filter(lambda x : x>0 ,a )),list(filter(lambda x : x<0 ,a )))
