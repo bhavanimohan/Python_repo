b0bf0a8964e01166bfb4dce6b2bfc9460ae30e76
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-08-21 14:00:00 +0530

    we had started REDUCE functions

--- higher_order_functions/filter.py
+++ higher_order_functions/filter.py
@@ -1,16 +1,23 @@
-a = ["nani","rajesh","jai","suresh","naresh"]
-b = list(filter(lambda x : len(x) > 4 , a))
-print(b)
+# a = ["nani","rajesh","jai","suresh","naresh"]
+# b = list(filter(lambda x : len(x) > 4 , a))
+# print(b)
 
-names = ["nani","aravind","ganesh","rajesh","jai","abhi","mahesh","avinash"]
-print(list(filter(lambda x : x.startswith("a") ,names)))
+# names = ["nani","aravind","ganesh","rajesh","jai","abhi","mahesh","avinash"]
+# print(list(filter(lambda x : x.startswith("a") ,names)))
 
-names = ["nani","aravind","ganesh","rajesh","jai","abhi","mahesh","avinash"]
-print(list(filter(lambda x : x.endswith("sh") ,names)))
-a = [1,-8,-4,7,8,5,-10,-6]
-print(list(filter(lambda x : x>0 ,a )))
-print(list(filter(lambda x : x<0 ,a )))
+# names = ["nani","aravind","ganesh","rajesh","jai","abhi","mahesh","avinash"]
+# print(list(filter(lambda x : x.endswith("sh") ,names)))
+# a = [1,-8,-4,7,8,5,-10,-6]
+# print(list(filter(lambda x : x>0 ,a )))
+# print(list(filter(lambda x : x<0 ,a )))
 
 
-a = [1,-8,-4,7,8,5,-10,-6]
-print(list(filter(lambda x : x>0 ,a )),list(filter(lambda x : x<0 ,a )))
+# a = [1,-8,-4,7,8,5,-10,-6]
+# print(list(filter(lambda x : x>0 ,a )),list(filter(lambda x : x<0 ,a )))
+
+
+# print("----------------------------- REDUCE -------------------------------")
+from functools import reduce
+nums = [1,2,3,4,5]
+res = reduce(lambda a, b: a if(a>b) else b , nums)
+print(res)
