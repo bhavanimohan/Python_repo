9d6b190fa1120f40b0b12e73135134672fa3454e
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-08-25 12:02:36 +0530

    doing some method operations on list comperhersions

--- list_comprhension/list-comperhension.py
+++ list_comprhension/list-comperhension.py
@@ -1,2 +1,18 @@
 nums = [i for i in range(1,11)]
 print(nums)
+
+sq = [i**2 for i in range(1,5)]
+print(sq)
+
+names = ["nani","anu","sai","hari"]
+res = [len(i) for i in names ]
+print(res)
+
+res = [i.upper() for i in names]
+print(res)
+
+res = [i.lower() for i in names]
+print(res)
+
+res = [i[0] for i in names]
+print(res)
