fb038dc1023de4acc19699adf5d3c072a5e6544c
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-08-26 15:34:28 +0530

    Done 8 problems from chatgpt

--- error_handling/error_handling.py
+++ error_handling/error_handling.py
@@ -1,49 +1,148 @@
-menu_item = {
-    "briyani" :170,
-    "mutton briyani" :210,
-    "coke" : 50,
-    "meals":120
-}
+# menu_item = {
+#     "briyani" :170,
+#     "mutton briyani" :210,
+#     "coke" : 50,
+#     "meals":120
+# }
 
-try:
-    name = input("Enter dish name  : ")
-    quantity  = int(input("Enter the quantity : "))
-    amount = int(input("Enter the amount : "))
-    item = menu_item[name]
-    total = item * quantity
-    discount = total / quantity
-    actual_amount = total - discount
-    return_amount = amount - total
+# try:
+#     name = input("Enter dish name  : ")
+#     quantity  = int(input("Enter the quantity : "))
+#     amount = int(input("Enter the amount : "))
+#     item = menu_item[name]
+#     total = item * quantity
+#     discount = total / quantity
+#     actual_amount = total - discount
+#     return_amount = amount - total
     
     
     
 
     
-except KeyError:
-    print("Enter a valid item from the menu.")
-except ZeroDivisionError:
-    print("Quantity shouldn't be Zero so minimun you need to add one.")
-    
-except Exception:
-    print("something happened go back to your code and check it.")
-except NameError:
-    print("Enter the valid name or valid number.")
+# except KeyError:
+#     print("Enter a valid item from the menu.")
+# except ZeroDivisionError:
+#     print("Quantity shouldn't be Zero so minimun you need to add one.")
+# except ValueError:
+#     print("Enter the a valid number.")
+# except NameError:
+#     print("Enter the valid name or valid number.")    
+# except Exception:
+#     print("something happened go back to your code and check it.")
+
     
         
     
     
     
     
+# else:
+#     print(f"Item you selected from the memu : {name}")
+#     print(f"The quantity you selected : {quantity}")
+#     print(f"Amount you given to waiter : {amount}")
+#     print(f"Total amount you need to pay : {total}")
+#     print(f"The discount you got on this item : {discount}")
+#     print(f"Return amount you got : {return_amount}")
+#     print(f"-------------------Payment successful : {actual_amount}-----------")
+#     print("---------------------------------Happpy shooppingggggg................")
+    
+    
+# finally:
+#     print("------------------------------------------Visit Again .... ")
+   
+   
+    
+# Write a program that asks the user to enter two numbers and divides the first number 
+# by the second. Handle ZeroDivisionError
+
+# try:
+#     name = input("Enter the valid name : ")
+#     amount = int(input("Enter a number :"))
+#     quantity = int(input("Enter the quantity : "))
+#     res = amount / quantity
+#     print(res)
+    
+# except ZeroDivisionError:
+#     print("You had entered invalid number so plz try again by running this code.")
+
+# Write a program that asks the user to enter an integer. 
+# If the user enters text instead of a number, handle ValueError
+
+# try:
+#     name = input("Enter the valid name : ")
+#     amount = int(input("Enter a number :"))
+#     quantity = int(input("Enter the quantity : "))
+#     res = amount / quantity
+#     print(res)
+    
+# except ValueError:
+#     print("You had entered invalid number so plz try again by running this code.")
+
+
+# Create a dictionary containing 5 food items and their prices. 
+# Ask the user for an item and handle KeyError if the item doesn't exist
+
+# menu = {
+#     "briyani" :170,
+#     "mutton briyani" :210,
+#     "coke" : 50,
+#     "meals":120,
+#     "diet coke": 70
+# }
+
+# try:
+#     name = input("Enter dish from menu : ")
+#     item = menu[name]
+#     print(item)
+# except KeyError:
+#     print("The selected item is not in the menu so try again...")
+
+
+# Modify the above program so that a 10% 
+# discount is given when the total bill is above ₹500.
+
+
+menu_item = {
+    "briyani" :170,
+    "mutton briyani" :210,
+    "coke" : 50,
+    "meals":120
+}
+try:
+    name = input("Enter dish name  : ")
+    quantity  = int(input("Enter the quantity : "))
+    amount = int(input("Enter the amount : "))
+    item = menu_item[name]
+    total = item * quantity
+    discount = 0
+    actual_amount = total 
+    return_amount = amount - total
+    if actual_amount > 500 :
+        actual_amount = actual_amount*10/100
+    actual_amount
+except KeyError:
+    print("Enter a valid item from the menu.")
+except ZeroDivisionError:
+    print("Quantity shouldn't be Zero so minimun you need to add one.")
+except ValueError:
+    print("Enter the a valid number.")
+except NameError:
+    print("Enter the valid name or valid number.")    
+except Exception:
+    print("something happened go back to your code and check it.")
+
 else:
     print(f"Item you selected from the memu : {name}")
     print(f"The quantity you selected : {quantity}")
     print(f"Amount you given to waiter : {amount}")
     print(f"Total amount you need to pay : {total}")
-    print(f"The discount you got on this item : {discount}")
+    print(f"The discount you got on this item : {actual_amount}")
     print(f"Return amount you got : {return_amount}")
-    print(f"-------------------Payment successful : {actual_amount}-----------")
+    print(f"-------------------Payment successful : {total-actual_amount}-----------")
     print("---------------------------------Happpy shooppingggggg................")
     
     
 finally:
     print("------------------------------------------Visit Again .... ")
+   
+   
