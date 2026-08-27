bcb143e48cd4593215776ea5f816f34c9a0d1d49
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-08-27 15:05:46 +0530

    Done with 5 problems

--- tasks/String_List_Tasks.py
+++ tasks/String_List_Tasks.py
@@ -1,18 +1,23 @@
-# Input = ["hello", "world", "python"]
-# empty_list = []
-# for i in Input:
-#     for j in i:
-#         empty_list=[j]+empty_list
-# print(empty_list)
+Input = ["hello", "world", "python"]
+empty_list = []
+for i in Input:
+    reverse = ""
+
+    for j in i:
+        reverse=j+reverse
+    empty_list.append(reverse)
+print(empty_list)
         
     
-# Input = ["madam", "hello", "level", "python", "radar"]
-# empty_list = []
-# for i in Input:
-#     for j in i:
-#         if i[j] == i[j-i]:
-#             empty_list+=[j]
-# print(empty_list)
+Input = ["madam", "hello", "level", "python", "radar"]
+empty_list = []
+for i in Input:
+    reverse = ""
+    for j in i:
+        reverse=j+reverse
+    if i == reverse:
+        empty_list.append(reverse)
+print(empty_list)
 
 
 Input = ["apple", "banana", "apple", "cat", "banana"]
@@ -21,4 +26,23 @@ for i in Input:
     if i not in empty_list:
         empty_list.append(i)
 print(empty_list)
+
+# Input = ["cat", "dog", "apple", "bat", "mango"]
+# len_3 = []
+# len_5 = []
+# for i in Input:
+#     if len(i) == 3:
+#         len_3.append(i)
+#     if len(i) == 5:
+#         len_5.append(i)
+
+
+a =  ["apple", "banana", "mango"]
+b = ["banana", "orange", "apple"]
+empty_list = []
+for char in a:
+    if char in b:
+        empty_list.append(char)
+print(empty_list)
+
     
