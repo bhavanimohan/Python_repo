88835d5d36ffa12460a7baa88bc5b43eba700e78
Bhavani Mohan <bhavanimohan200@gmail.com>
2026-08-31 13:28:47 +0530

    today we had seen how to connect python with sql

---
+++ python_sql/python_sql.py
@@ -0,0 +1,33 @@
+from pymysql import connect
+
+connection = connect(
+    host = "localhost",
+    user = "root",
+    password="Ammananna@12",
+    database="office"
+    
+)
+
+query = "select * from emps"
+cursor = connection.cursor()
+a = cursor.execute(query)
+res = cursor.fetchall()
+print(res)
+
+query1 = "select * from emps where id = 3"
+cursor = connection.cursor()
+b = cursor.execute(query1)
+res1 = cursor.fetchall()
+print(res1)
+
+query2 = "insert into emps(name,age,salary,location) values ('chandu',18,23000,'ckn')"
+cursor = connection.cursor()
+c = cursor.execute(query2)
+res2 = cursor.fetchall()
+print(res2)
+
+query = "select * from emps"
+cursor = connection.cursor()
+a = cursor.execute(query)
+res = cursor.fetchall()
+print(res)
