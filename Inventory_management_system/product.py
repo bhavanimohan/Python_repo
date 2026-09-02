96e18db1cf551e94bd306ba066646e518af820ab
bhavanimohan <114520808+bhavanimohan@users.noreply.github.com>
2026-09-02 20:02:45 +0530

    added some more code in the update part

--- Inventory_management_system/product.py
+++ Inventory_management_system/product.py
@@ -1,5 +1,7 @@
 from database import my_connection
 connection = my_connection()
+
+
 def get_product_details():
     connection = my_connection()
     cursor = connection.cursor()
@@ -9,9 +11,7 @@ def get_product_details():
     
 
 def print_products(products):
-
     for product in products:
-
         print("----------------------------------------")
         print(f"Product ID : {product[0]}")
         print(f"Name       : {product[1]}")
@@ -20,6 +20,8 @@ def print_products(products):
         print(f"Quantity   : {product[4]}")
         print(f"Supplier   : {product[5]}")
         print("----------------------------------------")
+        
+        
 def delete_product(product_id):
     user_selection = int(input("""Enter the number to delete by :
                                1. ID 
@@ -88,10 +90,55 @@ def add_product(name,category,price,quantity,supplier):
     print("Product added successfully!")
     cursor.close()
     connection.close()
-def update_product(product_id, name, category, price, quantity, supplier):
+    
+    
+def update_product():
+
+    
+    user_selection = int(input("""Enter the number to update by :
+                                1. Update all product details  
+                                2. ID 
+                                3. Name
+                                4. Price
+                                5. Category 
+                                6. Supplier """))
+    if user_selection == 1:
+        product_id = int(input("Enter the id to update : ")) 
+        name = input("Enter the name : ") 
+        category = input("Enter the category to update : ") 
+        price = float(input("Enter the price : "))
+        quantity = int(input("Enter the quantity : "))
+        supplier = input("Enter the supplier : ")
+        query = "UPDATE products SET name=%s, category=%s, price=%s, quantity=%s, supplier=%s WHERE id=%s"
+        values = (name, category, price, quantity, supplier, product_id)
+    if user_selection == 2:
+        product_id = int(input("Enter the id to update : "))
+        new_id = int(input("Enter the new id : "))
+        query = "UPDATE products SET id=%s WHERE id=%s"
+        values = (new_id, product_id)
+    if user_selection == 3: 
+        product_id = int(input("Enter the id to update : "))
+        new_name = input("Enter the new name : ")
+        query = "UPDATE products SET name=%s WHERE id=%s"
+        values = (new_name, product_id)
+    if user_selection == 4:
+        product_id = int(input("Enter the id to update : "))
+        new_price = float(input("Enter the new price : "))
+        query = "UPDATE products SET price=%s WHERE id=%s"
+        values = (new_price, product_id)
+    if user_selection == 5:
+        product_id = int(input("Enter the id to update : "))
+        new_category = input("Enter the new category : ")
+        query = "UPDATE products SET category=%s WHERE id=%s"
+        values = (new_category, product_id)
+    if user_selection == 6:
+        product_id = int(input("Enter the id to update : "))
+        new_supplier = input("Enter the new supplier : ")
+        query = "UPDATE products SET supplier=%s WHERE id=%s"
+        values = (new_supplier, product_id)
     connection = my_connection()
     cursor = connection.cursor()
-    cursor.execute("UPDATE products SET name=%s, category=%s, price=%s, quantity=%s, supplier=%s WHERE id=%s", (name, category, price, quantity, supplier, product_id))
+    cursor.execute(query, values)
     connection.commit()
     print("Product updated successfully!")
     cursor.close()
